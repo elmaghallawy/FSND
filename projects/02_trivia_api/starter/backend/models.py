f84d081f0e0ab5613f5085953691a4e9c629b4f3
Ahmed Elmaghallawy <ahmedelmaghallawy@protonmail.com>
2020-09-29 10:41:48 +0200

    connect to postgres

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -4,7 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+postgres_username = os.getenv('POSTGRES_USERNAME')  
+postgres_password = os.getenv('POSTGRES_PASSWORD')
+database_path = f"postgresql://{postgres_username}:{postgres_password}@localhost:5432/{database_name}"
 
 db = SQLAlchemy()
 
