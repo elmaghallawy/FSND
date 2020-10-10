0db95166f00b86f314557aa98b4adb746edcd043
Ahmed Elmaghallawy <ahmedelmaghallawy@protonmail.com>
2020-10-10 20:11:21 +0200

    fix auth0 url

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-12hz4kn5.eu.auth0.com', // the auth0 domain prefix
+    url: 'dev-12hz4kn5.eu', // the auth0 domain prefix
     audience: 'my-app', // the audience set for the auth0 app
     clientId: 'X6Dv6424VbmwHwKlFkIQfJSsfWZpt8eI', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
